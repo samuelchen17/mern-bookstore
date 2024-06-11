4fc911a437081c8ebb933055827d35a9b8cd172d
Samuel Chen <sche842@gmail.com>
2024-06-11 17:57:30 +1000

    remove config

--- server/config.js
+++ server/config.js
@@ -2,3 +2,5 @@ export const PORT = 5555;
 
 export const mongoDBURL =
   "mongodb+srv://root:root@book-store-mern.id7vddy.mongodb.net/?retryWrites=true&w=majority&appName=book-store-mern";
+
+// removed
