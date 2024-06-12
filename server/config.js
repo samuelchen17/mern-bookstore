929803ccfc5cd0d9101f98956db1dc5924072b52
Samuel Chen <sche842@gmail.com>
2024-06-12 12:17:36 +1000

    update

--- server/config.js
+++ server/config.js
@@ -2,5 +2,3 @@ export const PORT = 5555;
 
 export const mongoDBURL =
   "mongodb+srv://root:root@book-store-mern.id7vddy.mongodb.net/?retryWrites=true&w=majority&appName=book-store-mern";
-
-// removed
--- server/index.js
+++ server/index.js
@@ -5,16 +5,32 @@ import { Book } from "./models/bookModel.js";
 
 const app = express();
 
+// middleware for parsing request body
+app.use(express.json());
+
 app.get("/", (req, res) => {
   //   console.log(req);
   return res.status(234).send("welcome");
 });
 
 // Route for saving a new book
+// can use async keyword here for the callback function because mongoose is an async process
 app.post("/books", async (req, res) => {
   try {
     if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send({
+        message: "Send all required fields: title, author, publish year",
+      });
     }
+
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+    return response.status(201).send(book);
   } catch (err) {
     console.log(err.message);
     response.status(500).send({ message: err.message });
@@ -32,18 +48,3 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
-async function startServer() {
-  try {
-    await mongoose.connect(mongoDBURL);
-    console.log("App connected to database");
-
-    app.listen(PORT, () => {
-      console.log(`App is listening to port: ${PORT}`);
-    });
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-startServer();
