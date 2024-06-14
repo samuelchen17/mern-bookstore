caf12d85ee9daa4054695d818377ca8d30fd937b
Samuel Chen <sche842@gmail.com>
2024-06-14 15:44:47 +1000

    card switch

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Spinner from "../components/Spinner";
 import { Link } from "react-router-dom";
-import { AiOutlineEdit } from "react-icons/ai";
-import { BsInfoCircle } from "react-icons/bs";
-import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+import { MdOutlineAddBox } from "react-icons/md";
+import TableDisplay from "../components/home/TableDisplay";
+import CardDisplay from "../components/home/CardDisplay";
 
 function Home() {
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [displayType, setDisplayType] = useState("table");
 
   useEffect(() => {
     setLoading(true);
@@ -27,7 +28,25 @@ function Home() {
   }, []);
 
   return (
-    <div>
+    <div className="p-4">
+      <div className="flex justify-center items-center gap-x-4">
+        <button
+          className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg"
+          onClick={() => {
+            setDisplayType("table");
+          }}
+        >
+          Table
+        </button>
+        <button
+          className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg"
+          onClick={() => {
+            setDisplayType("card");
+          }}
+        >
+          Card
+        </button>
+      </div>
       <div className="flex justify-between items-center">
         <h1 className="text-3xl my-8">Books list</h1>
         <Link to="/books/create">
@@ -37,54 +56,10 @@ function Home() {
       {/* if loading = true, show loading icon, else generate the list */}
       {loading ? (
         <Spinner />
+      ) : displayType === "table" ? (
+        <TableDisplay books={books} />
       ) : (
-        <table className="w-full border-separate border-spacing-2">
-          <thead>
-            <tr>
-              <th className="border border-slate-600 rounded-md">No</th>
-              <th className="border border-slate-600 rounded-md">Title</th>
-              <th className="border border-slate-600 rounded-md max-md:hidden">
-                Author
-              </th>
-              <th className="border border-slate-600 rounded-md max-md:hidden">
-                Publish Year
-              </th>
-              <th className="border border-slate-600 rounded-md">Operations</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* map each book from books array to the list */}
-            {books.map((book, index) => (
-              <tr key={book._id} className="h-8">
-                <td className="border border-slate-700 rounded-md text-center">
-                  {index + 1}
-                </td>
-                <td className="border border-slate-700 rounded-md text-center">
-                  {book.title}
-                </td>
-                <td className="border border-slate-700 rounded-md text-center max-md:hidden">
-                  {book.author}
-                </td>
-                <td className="border border-slate-700 rounded-md text-center max-md:hidden">
-                  {book.publishYear}
-                </td>
-                <td className="border border-slate-700 rounded-md text-center">
-                  <div className="flex justify-center gap-x-4">
-                    <Link to={`/books/details/${book._id}`}>
-                      <BsInfoCircle className="text-2xl text-green-600" />
-                    </Link>
-                    <Link to={`/books/edit/${book._id}`}>
-                      <AiOutlineEdit className="text-2xl text-yellow-600" />
-                    </Link>
-                    <Link to={`/books/delete/${book._id}`}>
-                      <MdOutlineDelete className="text-2xl text-red-600" />
-                    </Link>
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <CardDisplay books={books} />
       )}
     </div>
   );
